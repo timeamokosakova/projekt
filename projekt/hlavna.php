0f15c6f5e7e9c5a060707858867524cc5748082e
Tímea Mokošáková <timea.mokosakova@gmail.com>
2018-04-08 22:30:57 +0200

    pridanie

---
+++ projekt/hlavna.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <LINK rel="stylesheet" type="text/css" href="styl.css">
+    <title>Doruèovacia služba</title>
+</head>
+
+    <a href="hlavna.html">
+        <img src="log.JPG" style="width:10%;height:40px">
+    </a>
+
+<body>
+
+
+    <DIV class=”menu”>
+        <table>
+            <tr>
+                <td><a href="index.php"> <img src="DOMOV.png" style="vertical-align: text-bottom;;width:40PX;height:40Px"> </a>
+                <td><A HREF="oSPOL.html"> O spoloènosti</A></td>
+                <td><A HREF="OB.html">Zásielka</A></td>
+                <td><A HREF="SZ.html"> Sledovanie zásielok </A></td>
+                <td><A HREF="EZ.html">Evidencia Zákazníkov</A></td>
+                <td><A HREF="kontakt.html">Kontakt</A></td>
+                <td><br></td>
+            </tr>
+        </table>
+
+        <DIV class="hlavna">
+            <CENTER><img src="posta.gif" style="vertical-align: text-bottom;;width:400PX;height:400PX0px"></CENTER>
+        </DIV>
+</body>
+        <p style="font-size:small; text-align:center;"> Tímea Mokošáková, 2018</style>
+</html>
