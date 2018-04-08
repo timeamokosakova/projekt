309a98382224290458bfd265d7a24f729aceb973
Tímea Mokošáková <timea.mokosakova@gmail.com>
2018-04-08 23:09:06 +0200

    spol

--- projekt/ospol.php
+++ projekt/ospol.php
@@ -1,3 +1,39 @@
-<?php
+<!DOCTYPE html>
+<html lang="SK-sk">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <LINK rel="stylesheet" type="text/css" href="styl.css">
+    <title>Doruèovacia sluba</title>
 
-?>
+    <a href="hlavna.html">
+        <img src="log.JPG" style="width:10%;height:40px">
+    </a>
+
+
+    <DIV class=”menu”>
+        <table>
+            <tr>
+                <td><a href="index.php"> <img src="DOMOV.png" style="vertical-align: text-bottom;width:40PX;height:40PX"> </a>
+                <td><A HREF="oSPOL.php"> O spoloènosti</A></td>
+                <td><A HREF="OB.php">Zásielka</A></td>
+                <td><A HREF="SZ.php"> Sledovanie zásielok </A></td>
+                <td><A HREF="EZ.php">Evidencia Zákazníkov</A></td>
+                <td><A HREF="kontakt.php>Kontakt</A></td>
+                <td><br></td>
+            </tr>
+        </table>
+
+        <DIV class="hlavna">
+
+            <UL type="disc">
+                <br>
+                <li>Spoloènos MDS so svojimi vlastnımi národnımi spoloènosami a partnermi pokrıva 10 európskych krajín. Kvalitná balíková sluba je našou najvyššou prioritou - viac ako 1000 zákazníkov sa na nás spolieha.</li>
+                <LI>Spoloènos vznikla v roku 2017. Za úèelom kvalitnej prepravy.</LI>
+                <br>
+                <br>
+                <br>
+            </UL>
+
+        </DIV>
+        <p style="font-size:small; text-align:center;"> Tímea Mokošáková, 2018</style>
+</html>
